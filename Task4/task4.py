4def3cd899d63e3382c3a698318e46468a67d3fd
Mariam Mohamed <141680346+mariamnail@users.noreply.github.com>
2023-08-26 20:37:42 +0300

    task4.py

--- Task4/task4.py
+++ Task4/task4.py
@@ -1,9 +1,8 @@
 import sys
 
-def get_command_line_arguments():
-    arguments = sys.argv[1:]  # Exclude the script name itself
+def get_arguments():
+    arguments = sys.argv[:]  
     return arguments
 
-# Usage example
-arguments = get_command_line_arguments()
+arguments = get_arguments()
 print("Command-line arguments:", arguments)
